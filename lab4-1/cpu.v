753494c9dadb8b25a03b1d225e4165d4e6720a28
colin31472 <colin31472@naver.com>
2024-04-30 00:56:54 +0900

    cpu update

--- lab4-1/cpu.v
+++ lab4-1/cpu.v
@@ -37,7 +37,7 @@ module cpu(input reset,       // positive reset signal
   //ImmGen
   wire [31:0] imm_gen_out;
   //HazardUnit
-  wire stall;
+  wire is_stall;
   //ALU, ALUCtrl
   wire [4:0] func_code;
   wire [31:0] alu_in_1;
@@ -46,6 +46,18 @@ module cpu(input reset,       // positive reset signal
   wire alu_bcond;
   //Dmem
   wire [31:0] dmem_out;
+
+  wire is_stall;
+
+  wire ID_EX_is_halted_temp;
+  //Forwarding
+  wire [1:0] forward_A;
+  wire [1:0] forward_B;
+  wire [31:0] forWard_B_out;
+  wire [1:0]mux_rs1_dout;
+  wire mux_rs2_dout;
+  wire [31:0] f_rs1_dout;
+  wire [31:0] f_rs2_dout;
 
   /***** Register declarations *****/
   // You need to modify the width of registers
@@ -68,6 +80,10 @@ module cpu(input reset,       // positive reset signal
   reg [31:0] ID_EX_imm;
   reg [31:0] ID_EX_ALU_ctrl_unit_input;
   reg [4:0] ID_EX_rd;
+  reg ID_EX_is_halted;
+
+  reg [4:0] ID_EX_rs1;
+  reg [4:0] ID_EX_rs2;
 
   /***** EX/MEM pipeline registers *****/
   // From the control unit
@@ -77,9 +93,10 @@ module cpu(input reset,       // positive reset signal
   reg EX_MEM_mem_to_reg;    // will be used in WB stage
   reg EX_MEM_reg_write;     // will be used in WB stage
   // From others
-  reg EX_MEM_alu_out;
-  reg EX_MEM_dmem_data;
-  reg EX_MEM_rd;
+  reg [31:0] EX_MEM_alu_out;
+  reg [31:0] EX_MEM_dmem_data;
+  reg [4:0] EX_MEM_rd;
+  reg EX_MEM_is_halted;
 
   /***** MEM/WB pipeline registers *****/
   // From the control unit
@@ -88,6 +105,22 @@ module cpu(input reset,       // positive reset signal
   // From others
   reg MEM_WB_mem_to_reg_src_1;
   reg MEM_WB_mem_to_reg_src_2;
+
+  reg [4:0] MEM_WB_rd;
+  reg MEM_WB_is_halted;
+
+
+  assign rs2 = IF_ID_inst[24:20];
+  assign rd = MEM_WB_rd;
+  assign ID_EX_is_halted_temp = is_ecall & (f_rs1_dout==10)&(rs1==17);
+  assign is_halted = MEM_WB_is_halted;
+
+  Mux2to1 mux_is_ecall(
+    .in0(IF_ID_inst[19:15]),
+    .in1(5'b10001),
+    .sell(is_ecall),
+    .out(rs1)
+  );
 
   // ---------- Update program counter ----------
   // PC must be updated on the rising edge (positive edge) of the clock.
@@ -158,7 +191,7 @@ module cpu(input reset,       // positive reset signal
 
   // Update ID/EX pipeline registers here
   always @(posedge clk) begin
-    if (reset | stall) begin
+    if (reset | is_stall) begin
       // From ControlUnit
       ID_EX_alu_op <= 0;         
       ID_EX_alu_src <= 0;        
@@ -172,6 +205,9 @@ module cpu(input reset,       // positive reset signal
       ID_EX_imm <= 0;
       ID_EX_ALU_ctrl_unit_input <= 0;
       ID_EX_rd <= 0;
+      ID_EX_is_halted <= 0;
+      ID_EX_rs1 <= 0;
+      ID_EX_rs2 <= 0;
     end
     else begin
       // From ControlUnit
@@ -182,17 +218,20 @@ module cpu(input reset,       // positive reset signal
       ID_EX_mem_to_reg <= mem_to_reg;   
       ID_EX_reg_write <= reg_write;     
       // From others
-      ID_EX_rs1_data <= rs1_dout;
-      ID_EX_rs2_data <= rs2_dout;
+      ID_EX_rs1_data <= f_rs1_dout;
+      ID_EX_rs2_data <= f_rs2_dout;
       ID_EX_imm <= imm_gen_out;
       ID_EX_ALU_ctrl_unit_input <= IF_ID_inst;
       ID_EX_rd <= IF_ID_inst[11:7];
+      ID_EX_is_halted <= ID_EX_is_halted_temp;
+      ID_EX_rs1 <= rs1;
+      ID_EX_rs2 <= rs2;
     end
   end
 
   // ---------- ALU Control Unit ----------
   ALUControlUnit alu_ctrl_unit (
-    .part_of_inst(),  // input
+    .part_of_inst(ID_EX_ALU_ctrl_unit_input),  // input
     .opcode(ID_EX_alu_op),
     .alu_op(func_code)         // output
   );
@@ -218,6 +257,7 @@ module cpu(input reset,       // positive reset signal
       EX_MEM_alu_out <= 0;
       EX_MEM_dmem_data <= 0;
       EX_MEM_rd <= 0;
+      EX_MEM_is_halted <= 0;
     end
     else begin
       //From ControlUnit
@@ -229,6 +269,7 @@ module cpu(input reset,       // positive reset signal
       EX_MEM_alu_out <= alu_result;
       EX_MEM_dmem_data <= forWard_B_out;
       EX_MEM_rd <= ID_EX_rd;
+      EX_MEM_is_halted <= ID_EX_is_halted;
     end
   end
 
@@ -252,6 +293,8 @@ module cpu(input reset,       // positive reset signal
       //From others
       MEM_WB_mem_to_reg_src_1 <= 0;
       MEM_WB_mem_to_reg_src_2 <= 0;
+      MEM_WB_is_halted <= 0;
+      MEM_WB_rd <= 0;
     end
     else begin
       //From ControlUnit
@@ -260,6 +303,8 @@ module cpu(input reset,       // positive reset signal
       //From others
       MEM_WB_mem_to_reg_src_1 <= dmem_out;
       MEM_WB_mem_to_reg_src_2 <= EX_MEM_alu_out;
+      MEM_WB_is_halted <= EX_MEM_is_halted;
+      MEM_WB_rd <= EX_MEM_rd;
     end
   end
 
@@ -267,17 +312,80 @@ module cpu(input reset,       // positive reset signal
     .in0(MEM_WB_mem_to_reg_src_1), 
     .in1(MEM_WB_mem_to_reg_src_2), 
     .sel(MEM_WB_mem_to_reg), 
-    .out(Reg_Write_MUX_Out)
+    .out(rd_din)
   );
 
   ForwardingUnit FU(
+    .ID_EX_rs1(ID_EX_rs1),
+    .ID_EX_rs2(ID_EX_rs2),
+    .EX_MEM_rd(EX_MEM_rd),
+    .MEM_WB_rd(MEM_WB_rd),
+    .EX_MEM_RegWrite(EX_MEM_reg_write),
+    .MEM_WB_RegWrite(MEM_WB_reg_write),
+    .ForwardA(forward_A),
+    .ForwardB(forward_B)
+  );
 
-  )
+  Mux4to1 mux_forward_A(
+    .in0(ID_EX_rs1_data),
+    .in1(EX_MEM_alu_out),
+    .in2(rd_din),
+    .in3(0),
+    .sel(forward_A),
+    .out(alu_in_1)
+  );
+
+  Mux4to1 mux_forward_B(
+    .in0(ID_EX_rs2_data),
+    .in1(EX_MEM_alu_out),
+    .in2(rd_din),
+    .in3(0),
+    .sel(forward_B),
+    .out(forWard_B_out)
+  );
+
+  Mux2to1 mux_ID_EX_alu_src(
+    .in0(forWard_B_out),
+    .in1(ID_EX_imm),
+    .sel(ID_EX_alu_src),
+    .out(alu_in_2)
+  );
+
+  ForwardingEcallUnit FEU(
+    .rs1(rs1), 
+    .rs2(rs2),
+    .fromWBrd(rd),
+    .EX_MEM_rd(EX_MEM_rd),
+    .is_ecall(is_ecall),
+    .mux_rs1_dout(mux_rs1_dout),
+    .mux_rs2_dout(mux_rs2_dout)
+  );
+
+  Mux4to1 Mux_rs1_dout(
+    .in0(rd_din),
+    .in1(rs1_dout),
+    .in2(EX_MEM_alu_out),
+    .in3(0),
+    .sel(mux_rs1_dout),
+    .out(f_rs1_dout)
+  );  
+
+  Mux2to1 Mux_rs2_dout(
+    .in0(rd_din),
+    .in1(rs2_dout),
+    .sel(mux_rs2_dout),
+    .out(f_rs2_dout)
+  );
 
   HazardDetectionUnit HDU(
-
-  )
-
+    .rs1(rs1),
+    .rs2(rs2),
+    .ID_EX_rd(ID_EX_rd),
+    .EX_MEM_rd(EX_MEM_rd),
+    .ID_EX_mem_read(ID_EX_mem_read),
+    .EX_MEM_mem_read(EX_MEM_mem_read),
+    .is_ecall(is_ecall),
+    .is_stall(is_stall)
+  );
   
 endmodule
-//is_halted, Forwarding, Hazard, mux 연결X
