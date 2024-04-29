c3435bad180872c93714108d97a3b0294b7ba3e1
a-nodi <leeyoonhyuk0@naver.com>
2024-04-30 03:16:30 +0900

    [FIX] minor bux fix in ecallunit

--- lab4-1/ForwardingEcallUnit.v
+++ lab4-1/ForwardingEcallUnit.v
@@ -10,17 +10,17 @@ module ForwardingEcallUnit(
     mux_rs1_sel,
     mux_rs2_sel
 );
-    input rs1;
-    input rs2;
+    input [4:0] rs1;
+    input [4:0] rs2;
     input is_ecall;
     input fromWBrd;
-    input EX_MEM_rd;
+    input [4:0] EX_MEM_rd;
     output reg [1:0] mux_rs1_sel;
     output reg mux_rs2_sel;
     
     always @(*) begin
         
-        if (EX_MEM_rd==17 && is_ecall) begin
+        if (EX_MEM_rd == 17 && is_ecall) begin
             mux_rs1_sel = `RD_DIN_SEL;
         end
         else begin
