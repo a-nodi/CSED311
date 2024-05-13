4e999a83c0f5ebef0573007ab41b5f55086a3dc9
a-nodi <leeyoonhyuk0@naver.com>
2024-05-13 22:58:54 +0900

    [Fix] Fixed tag length

--- lab4-2/Constants.v
+++ lab4-2/Constants.v
@@ -1,3 +1,6 @@
 `define INSTRUCTION_WIDTH 32
-`define TAG_WIDTH 27
+`define TAG_WIDTH 25
 `define BTB_INDEX_WIDTH 5  // As the TA said.
+
+// pc : [31:7] [6:2] [1:0]
+//       tag  index  offset <- offset doesn't change since pc is always incremented by 4
