8254de1f36382edc1a8d564b6e462333f44e648e
a-nodi <leeyoonhyuk0@naver.com>
2024-05-13 21:06:05 +0900

    [Feat] Implemeted tag table

--- lab4-2/Constants.v
+++ lab4-2/Constants.v
@@ -1,3 +1,3 @@
 `define INSTRUCTION_WIDTH 32
-`define TAG_WIDTH 5
-`define BTB_INDEX_WIDTH 27
+`define TAG_WIDTH 27
+`define BTB_INDEX_WIDTH 5  // As the TA said.
--- lab4-2/TagTable.v
+++ lab4-2/TagTable.v
@@ -2,12 +2,24 @@
 
 module TagTable(
     btb_index,
+    reset,
     corresponding_tag
 );
     input [`BTB_INDEX_WIDTH - 1:0] btb_index;
+    input reset;
     output reg [`TAG_WIDTH - 1:0] corresponding_tag;
 
+    reg [2**`BTB_INDEX_WIDTH - 1:0] tag_table[0 : 2**`BTB_INDEX_WIDTH - 1];
+    integer i;
 
+    always @(posedge reset) begin
+        for (i = 0; i < 2**`BTB_INDEX_WIDTH; i = i + 1) begin
+            tag_table[i] = 0;
+        end
+    end
 
+    always @(*) begin
+        corresponding_tag = tag_table[btb_index];
+    end
 
 endmodule
