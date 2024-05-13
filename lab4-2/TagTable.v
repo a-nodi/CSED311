70d217e43459cc5b8454f8c9da608f59b34294a7
a-nodi <leeyoonhyuk0@naver.com>
2024-05-13 22:20:44 +0900

    [Feat] Added tag refreshing part

--- lab4-2/TagTable.v
+++ lab4-2/TagTable.v
@@ -2,24 +2,35 @@
 
 module TagTable(
     btb_index,
+    current_tag,
+    clk,
     reset,
     corresponding_tag
 );
     input [`BTB_INDEX_WIDTH - 1:0] btb_index;
+    input [`TAG_WIDTH - 1:0] current_tag;
+    input clk;
     input reset;
     output reg [`TAG_WIDTH - 1:0] corresponding_tag;
 
     reg [2**`BTB_INDEX_WIDTH - 1:0] tag_table[0 : 2**`BTB_INDEX_WIDTH - 1];
     integer i;
 
-    always @(posedge reset) begin
-        for (i = 0; i < 2**`BTB_INDEX_WIDTH; i = i + 1) begin
-            tag_table[i] = 0;
-        end
-    end
-
     always @(*) begin
         corresponding_tag = tag_table[btb_index];
+    end
+    
+    // Write the tag into the table when posedge clk
+    always @(posedge clk) begin
+        // Initialize the table to 0
+        if (reset) begin
+            for (i = 0; i < 2**`BTB_INDEX_WIDTH; i = i + 1) begin
+                tag_table[i] <= 0;
+            end
+        end
+        
+        // Write the tag into the table
+        tag_table[btb_index] <= current_tag;
     end
 
 endmodule
